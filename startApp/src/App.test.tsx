61aaeac03614cc3e96249c27c1833bf13dd18f54
dev-admin-at-646815525187 <Fsgys4KIxc+IXa+TA9o28vomkeG5Ws+dyXbrxZdXDik=>
2024-10-10 14:17:04 +0300

    Added basic configuration

---
+++ startApp/src/App.test.tsx
@@ -0,0 +1,14 @@
+import { describe, expect, it, test } from 'vitest';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('demo', () => {
+  expect(true).toBe(true);
+});
+
+describe('render', () => {
+  it('renders the main page', () => {
+    render(<App />);
+    expect(true).toBeTruthy();
+  });
+});
